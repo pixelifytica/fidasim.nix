393e2c587c29ccd4351d5eb9b7bf8ad37c25f1f9
Evie Litherland-Smith <evie.litherland-smith@ukaea.uk>
2026-05-22 14:43:10 +0100

    Re-write to build bundled HDF5 properly
    Use bundled HDF5 as dependency to FIDASIM derivation

--- flake.nix
+++ flake.nix
@@ -9,57 +9,72 @@
       pkgs = forAllSystems (system: import nixpkgs { inherit system; });
     in
     {
-      packages = forAllSystems (system: {
-        default = self.packages.${system}.fidasim;
-        hdf5 =
-          (pkgs.${system}.hdf5_1_10.override {
-            cppSupport = false;
-            enableShared = false;
-          }).overrideAttrs
-            (prev: {
-              nativeBuildInputs = prev.nativeBuildInputs ++ [ pkgs.${system}.gfortran ];
-              dontDisableStatic = true;
-              configureFlags = prev.configureFlags ++ [ "--enable-fortran" ];
-            });
-        fidasim = pkgs.${system}.stdenv.mkDerivation (final: {
-          pname = "fidasim";
+      packages = forAllSystems (
+        system:
+        let
           version = "2.0.0";
           src = pkgs.${system}.fetchFromGitHub {
             owner = "D3DEnergetic";
             repo = "FIDASIM";
-            tag = "v${final.version}";
+            tag = "v${version}";
             hash = "sha256-/w5dqCwqn2bIsTi6ieLdKZb/uAHFvsB7S4s72S02pNI=";
           };
-          patches = [ ./scipy.patch ];
-          nativeBuildInputs =
-            (with pkgs.${system}; [
+        in
+        {
+          hdf5 = pkgs.${system}.stdenvNoCC.mkDerivation (final: {
+            pname = "hdf5";
+            version = "1.8.16";
+            src = "${src}/deps/hdf5-${final.version}.tar.gz";
+            nativeBuildInputs = with pkgs.${system}; [
+              gcc13
               gnumake
               gfortran
               zlib
-              python3
-              git
-            ])
-            ++ [ self.packages.${system}.hdf5 ];
-          makeFlags = [
-            "HDF5_LIB=${self.packages.${system}.hdf5.out}/lib"
-            "HDF5_INCLUDE=${self.packages.${system}.hdf5.dev}/include"
-          ];
-          buildFlags = [
-            "src"
-            "tables"
-          ];
-          installPhase = ''
-            mkdir -p $out/bin
-            cp fidasim tables/generate_tables $out/bin
-            cp -R lib tables test docs $out
-          '';
-          meta = {
-            homepage = "https://d3denergetic.github.io/FIDASIM/index.html";
-            licence = pkgs.lib.licence.bsd3;
-            mainProgram = "fidasim";
-          };
-        });
-      });
+            ];
+            configureFlags = [ "--enable-fortran" ];
+            dontDisableStatic = true;
+            postPatch = ''
+              substituteInPlace configure --replace-fail "/bin/mv" "mv"
+              substituteInPlace configure.ac --replace-fail "/bin/mv" "mv"
+            '';
+          });
+          fidasim = pkgs.${system}.stdenv.mkDerivation (final: {
+            inherit src;
+            pname = "fidasim";
+            version = "2.0.0";
+            patches = [ ./scipy.patch ];
+            buildInputs =
+              (with pkgs.${system}; [
+                gnumake
+                gfortran
+                zlib
+                git
+              ])
+              ++ [ self.packages.${system}.hdf5 ];
+            propagatedNativeBuildInputs = [ pkgs.${system}.python3 ];
+            makeFlags = [
+              "FC=gfortran"
+              "HDF5_LIB=${self.packages.${system}.hdf5}/lib"
+              "HDF5_INCLUDE=${self.packages.${system}.hdf5}/include"
+            ];
+            buildFlags = [
+              "src"
+              "tables"
+            ];
+            installPhase = ''
+              mkdir -p $out/bin
+              cp fidasim tables/generate_tables $out/bin
+              cp -R lib tables test docs $out
+            '';
+            meta = {
+              homepage = "https://d3denergetic.github.io/FIDASIM/index.html";
+              licence = pkgs.lib.licence.bsd3;
+              mainProgram = "fidasim";
+            };
+          });
+          default = self.packages.${system}.fidasim;
+        }
+      );
       devShells = forAllSystems (system: {
         default = pkgs.${system}.mkShellNoCC {
           packages = [
